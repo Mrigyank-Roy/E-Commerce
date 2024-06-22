1a204d6f0d49ba1a73e6a520672468c51131a513
Mrigyank_Roy <roymrigyank2004@gmail.com>
2024-06-22 19:21:12 +0530

    E-Commerce Website

---
+++ scripts/checkout.js
@@ -0,0 +1,9 @@
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import '../data/backend-practice.js';
+import {products, loadProducts} from '../data/products.js';
+
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
